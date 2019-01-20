4e2d0a8b3c7547393cf1d228ab9a02a0656932ce
kooroshrz <knasa.kr@aut.ac.ir>
2019-01-20 14:05:27 +0330

    some improvement & refactoring for styles

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.master')
 @section('main content')
 
-    <link rel="stylesheet" href="/css/index_users.css">
+    <link rel="stylesheet" href="/css/index_games.css">
     <ol>
         @foreach($games as $game)
             <div class="game">
-                <img class="d-block w-25" src="/images/games/{{ $game->id }}.png" alt="game{{ $game->id }}" style="border-radius: 5px 5px 5px 5px">
+                <img class="d-block w-25" src="/images/games/{{ $game->id }}.png" alt="game{{ $game->id }}">
                 <ul>
                     <li>MAX SCORE: {{ $game->maxScore }}</li>
                     <li> ZERO MAKER: {{ $game->zeroMaker }}</li>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,15 +2,15 @@
 
 @section('main content')
 
-    <link rel="stylesheet" href="/css/index_users.css">
+    <link rel="stylesheet" href="/css/index_users.css" type="text/css">
 
     <ol>
         @foreach($users as $user)
             <div class="user">
-                <img class="d-block w-25" src="/images/users/{{ $user->username }}.jpg" alt="{{$user->username}}" style="border-radius: 5px 5px 5px 5px">
+                <img class="d-block w-25" src="/images/users/{{ $user->username }}.jpg" alt="{{$user->username}}">
                 <br>
                 <ul>
-                    <li> USERNAME: <a href="/users/{{ $user->username }}">{{ $user->username }}</li></a>
+                    <li> USERNAME: <a href="/users/{{ $user->username }}">{{ $user->username }}</a></li>
                     <li>stars: {{ $user->stars }}</li>
                     <li>TOTAL GAMES PLAYED: {{ $user->gamesPlayed }}</li>
                     <li>TOTAL GAMES WINED: {{ $user->gamesWined }}</li>
